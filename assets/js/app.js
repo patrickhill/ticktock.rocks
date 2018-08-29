dbd8dc45412bb7a5ee3d4272908e154b69eba1ad
Patrick Hill <patrickrhill@gmail.com>
2018-08-28 21:16:53 -0400

    stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
-document.addEventListener('gesturestart', function (e) {
-    e.preventDefault();
-});
+// document.addEventListener('gesturestart', function (e) {
+//     e.preventDefault();
+// });
 
-var timerText = document.getElementsByClassName('text-holder')[0];
-var timerDuration = getComputedStyle(document.body).getPropertyValue('--timer-duration');
+// var timerText = document.getElementsByClassName('text-holder')[0];
+// var timerDuration = getComputedStyle(document.body).getPropertyValue('--timer-duration');
 
-var compiledText = "<div>timer set for</div>" + timerDuration
-timerText.innerHTML = compiledText;
+// var compiledText = "<div>timer set for</div>" + timerDuration
+// timerText.innerHTML = compiledText;
+
+document.getElementById('timerValue').value = '120';
