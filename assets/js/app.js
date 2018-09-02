866731921102e7f2a9e1c042654c69b6d442ad39
Patrick Hill <patrickrhill@gmail.com>
2018-09-02 16:10:09 -0400

    makes play pause work

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,17 @@
 // timerText.innerHTML = compiledText;
 
 document.getElementById('timerValue').value = '120';
+
+var pausePlay = document.getElementById('pause-play');
+var animations = document.getElementsByClassName('animates');
+
+pausePlay.addEventListener("click", function( event ) {
+  Array.prototype.forEach.call(animations, function(el) {
+	    // console.log(el.style.WebkitAnimationPlayState);
+	    if (el.style.WebkitAnimationPlayState === "paused") {
+	    	el.style.WebkitAnimationPlayState = "running";
+	    } else {
+	    	el.style.WebkitAnimationPlayState = "paused";
+	    }
+	});
+}, false);
